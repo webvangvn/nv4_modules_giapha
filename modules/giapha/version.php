7b1d5814700fc99cb01d6b2c061ad5f04de90a1c
webvangvn <adminwmt@gmail.com>
2015-05-19 15:55:03 +0700

    change value version

--- modules/giapha/version.php
+++ modules/giapha/version.php
@@ -7,17 +7,21 @@
  * @Createdate 08/10/2014
  */
 
-if ( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
+ 
+if( ! defined( 'NV_ADMIN' ) or ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
 
-$module_version = array( //
-		"name" => "Giapha", //
-		"modfuncs" => "main,detail,anniversary,genealogy,shows,location,manager", //
-		"is_sysmod" => 0, //
-		"virtual" => 1, //
-		"version" => "4.0.00", //
-		"date" => "Thu, 02 Apr 2015 08:03:48 GMT", //
-		"author" => "CLB NukeViet HCMC (hoang.nguyen@webvang.vn)", //
-		"uploads_dir" => array($module_name), //
-		"note" => "hoang.nguyen@webvang.vn" //
-	);
+$module_version = array( 
+	'name' => 'Giapha', //
+	'modfuncs' => 'main,detail,anniversary,genealogy,shows,location,manager', //
+	'change_alias' => 'topic,groups,content,rss',
+	'submenu' => 'content,rss,search',
+	'is_sysmod' => 0, //
+	'virtual' => 1, //
+	'version' => '4.0.00', //
+	'date' => 'Thu, 02 Apr 2015 08:03:48 GMT', //
+	'author' => 'CLB NukeViet HCMC (hoang.nguyen@webvang.vn)', //
+	'note' => '', //
+	'uploads_dir' => array( $module_name ),
+	'files_dir' => array( $module_name )
+);
 
